b8ad2e302cbdf2fc9947347520b9c7f5a54ed00b
Ian Machado <89645823+IanMachado@users.noreply.github.com>
2022-02-01 12:03:50 -0300

    Update Procurar um produto na lista.py

--- Procurar um produto na lista.py
+++ Procurar um produto na lista.py
@@ -1,18 +1,6 @@
 product = ['tv', 'geladeira', 'guarda roupa', 'fogão', 'sofá', 'celular', 'notebook']
 stock = ['50', '20', '35', '60', '45', '150', '200']
 
-# relacionando um estoque com seu índice
-
-##print(estoque[2])
-# tem 35 guarda roupa, mas e se eu não soubesse o índice do guarda roupa? em uma lista grande
-
-i = product.index('guarda roupa')
-qtd_stock = stock[i]
-# index irá achar o índice do item na lista, que nesse caso é 2
-
-#print(f'A quantidade em estoque de guarda-roupa é {qtd_estoque}')
-
-
 name_of_product = input('name of product: ').lower()
 
 if name_of_product not in product:
