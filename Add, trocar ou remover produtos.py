b1be3c9f4502b0ac819bc2263eb5b2a8fe91f6c1
Ian Machado <ianqmachado4035@gmail.com>
2022-02-03 17:28:48 -0300

    try, except, lista, if, elif e else

--- Add, trocar ou remover produtos.py
+++ Add, trocar ou remover produtos.py
@@ -6,32 +6,34 @@
 
 lista = ['iphone 11', 'apple watch', 'airpods', 'ipad', 'mac book', 'apple tv', 'iphone 12']
 
-#item_removido = lista.pop(0)
-#print(lista)
-#print(f'Excluímos o item {item_removido} da lista')
+# item_removido = lista.pop(0)
+# print(lista)
+# print(f'Excluímos o item {item_removido} da lista')
 print(lista)
 opcao = input('Deseja Adicionar, Trocar ou Remover um item da lista? ')
 opcao = opcao.lower()
 
 if opcao == 'adicionar':
-   produto = input('Nome do novo produto: ').lower()
-   lista.append(produto)
+    produto = input('Nome do novo produto: ').lower()
+    lista.append(produto)
 
 elif opcao == 'trocar':
-   produto_removido = input('Nome do produto que será trocado: ').lower()
-   while produto_removido not in lista:
-       print('Produto não encontrado, tente novamente!')
-       produto_removido = input('Nome do produto que será trocado: ')
-   i = lista.index(produto_removido)
-   produto_adicionado = input('Nome do produto que será adicionado no lugar: ').lower()
-   lista[i] = produto_adicionado
+    produto_removido = input('Nome do produto que será trocado: ').lower()
+    try:
+        i = lista.index(produto_removido)
+        produto_adicionado = input('Nome do produto que será adicionado no lugar: ').lower()
+        lista[i] = produto_adicionado
+    except:
+        print('Produto não encontrado, tente novamente!')
+        quit()
 
 elif opcao == 'remover':
-    produto_removido = input('Nome do produto que será removido: ')
-    while produto_removido not in lista:
+    try:
+        produto_removido = input('Nome do produto que será removido: ').lower()
+        lista.remove(produto_removido)
+    except:
         print('Produto não encontrado, tente novamente')
-        produto_removido = input('Nome do produto que será removido: ')
-    lista.remove(produto_removido)
+        quit()
 
 else:
     print('Comando inválido, tente novamente')
